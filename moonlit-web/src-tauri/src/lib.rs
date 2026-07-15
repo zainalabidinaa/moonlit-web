7ba569935fb48e92f38599f1a04ba75406022006
Zain Alabidin Alkafaji <zainalkafaji20@gmail.com>
2026-07-16 01:49:48 +0200

    feat(desktop): scaffold Tauri 2 shell with frameless window and moonlit:// scheme

---
+++ moonlit-web/src-tauri/src/lib.rs
@@ -0,0 +1,18 @@
+pub fn run() {
+    let mut builder = tauri::Builder::default();
+
+    #[cfg(desktop)]
+    {
+        builder = builder.plugin(tauri_plugin_single_instance::init(|app, _args, _cwd| {
+            use tauri::Manager;
+            if let Some(window) = app.get_webview_window("main") {
+                let _ = window.set_focus();
+            }
+        }));
+    }
+
+    builder
+        .plugin(tauri_plugin_deep_link::init())
+        .run(tauri::generate_context!())
+        .expect("error while running tauri application");
+}
